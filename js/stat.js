9c5f68a4e732a2b7d016775486ab458d74089ec9
Maksim <mouxez@gmail.com>
2020-01-29 19:50:08 +0300

    creates variables for all values

--- js/stat.js
+++ js/stat.js
@@ -3,41 +3,49 @@
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var BAR_WIDTH = 40;
+var FONT_GAP = 16;
+var COLUMN_GAP = 50;
+var barHeight = CLOUD_HEIGHT - 4 * FONT_GAP - 6 * GAP;
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 window.renderStatistics = function (ctx) {
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, 100, 10, '#fff');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 130, 35);
-  ctx.fillText('Список результатов:', 130, 55);
+  ctx.fillText('Ура вы победили!', CLOUD_X + 4 * GAP, CLOUD_Y + GAP * 3);
+  ctx.fillText('Список результатов:', CLOUD_X + 4 * GAP, CLOUD_Y + GAP * 4 + FONT_GAP);
 
   ctx.fillStyle = '#000';
-  ctx.fillText('You', 160, 270);
-  ctx.fillText('2345', 160, 90);
+  ctx.fillText('1200', CLOUD_X + COLUMN_GAP + GAP, CLOUD_X - GAP);
+  ctx.fillText('You', CLOUD_X + COLUMN_GAP + GAP, CLOUD_HEIGHT);
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(160, 100, 40, 150);
+  ctx.fillRect(CLOUD_X + COLUMN_GAP + GAP, CLOUD_X, BAR_WIDTH, barHeight);
 
   ctx.fillStyle = '#000';
-  ctx.fillText('Ann', 250, 270);
-  ctx.fillText('1234', 250, 140);
+  ctx.fillText('1200', CLOUD_X + 2 * COLUMN_GAP + GAP + BAR_WIDTH, CLOUD_X - GAP);
+  ctx.fillText('Ann', CLOUD_X + 2 * COLUMN_GAP + GAP + BAR_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = '#0770ff';
-  ctx.fillRect(250, 150, 40, 100);
+  ctx.fillRect(CLOUD_X + 2 * COLUMN_GAP + GAP + BAR_WIDTH, CLOUD_X, BAR_WIDTH, barHeight);
 
   ctx.fillStyle = '#000';
-  ctx.fillText('Max', 340, 270);
-  ctx.fillText('1534', 340, 120);
+  ctx.fillText('1200', CLOUD_X + 3 * COLUMN_GAP + GAP + 2 * BAR_WIDTH, CLOUD_X - GAP);
+  ctx.fillText('Max', CLOUD_X + 3 * COLUMN_GAP + GAP + 2 * BAR_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = '#0990ff';
-  ctx.fillRect(340, 130, 40, 120);
+  ctx.fillRect(CLOUD_X + 3 * COLUMN_GAP + GAP + 2 * BAR_WIDTH, CLOUD_X, BAR_WIDTH, barHeight);
 
   ctx.fillStyle = '#000';
-  ctx.fillText('Alex', 430, 270);
-  ctx.fillText('1734', 430, 110);
+  ctx.fillText('1200', CLOUD_X + 4 * COLUMN_GAP + GAP + 3 * BAR_WIDTH, CLOUD_X - GAP);
+  ctx.fillText('Alex', CLOUD_X + 4 * COLUMN_GAP + GAP + 3 * BAR_WIDTH, CLOUD_HEIGHT);
   ctx.fillStyle = '#0350ff';
-  ctx.fillRect(430, 120, 40, 130);
+  ctx.fillRect(CLOUD_X + 4 * COLUMN_GAP + GAP + 3 * BAR_WIDTH, CLOUD_X, BAR_WIDTH, barHeight);
 };
